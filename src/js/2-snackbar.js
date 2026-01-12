14a0b828459eafad017de8da1ec8a86b3823332e
Tania Diachok <tetianadiachok14@gmail.com>
2026-01-12 17:45:35 +0100

    add hw

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,51 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const delay = Number(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  // ===== 🔥 ВАЛІДАЦІЯ =====
+  if (isNaN(delay) || delay < 0) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Delay must be a positive number',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+}
